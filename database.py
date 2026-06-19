e28283e27d8504e822b01da83542bd624b4cc724
PavanSurisetti <161280590+PavanSurisetti@users.noreply.github.com>
2026-06-19 11:38:42 +0530

    Enhance database engine connection settings
    Added SSL mode and pre-ping to database engine connection.

--- database.py
+++ database.py
@@ -8,7 +8,11 @@ db_url=os.getenv('db_url')#this will import the db_url file
 if not db_url:#to handle if no db_url found in .env file
     raise ValueError("db_url not found in .env file")
 #create a engine
-engine=create_engine(db_url)#this will establish connection to postgresql
+engine = create_engine(
+    db_url,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
+)#this will establish connection to postgresql
 #creating a base class for all the tables
 Base=declarative_base()
 #session factory
